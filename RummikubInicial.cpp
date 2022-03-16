df41a99f204de04249f3e36484e10a09020ffba3
JavierRDA01 <81706622+JavierRDA01@users.noreply.github.com>
2022-03-16 19:25:57 +0100

    Update RummikubInicial.cpp

--- RummikubInicial.cpp
+++ RummikubInicial.cpp
@@ -20,11 +20,11 @@ struct tFicha {
 	tColor color;
 };
 
-typedef tFicha tArrayFichas[MaxFichas];
+typedef tFicha tArray[MaxFichas];
 
 struct tSoporte {
 	int contador = 0;
-	tArrayFichas ficha;
+	tArray ficha;
 };
 
 typedef tSoporte tSoportes[NumJugadores];
@@ -46,13 +46,17 @@ struct tTablero
 	int contador = 0;
 };
 
+int menu();
 void inicializarBolsa(tBolsa& bolsa);
-
 tFicha robar(tBolsa& bolsa);
+bool recorrerBolsa(tBolsa& bolsa, int& fila, int& columna, tFicha& ficha);
+void repartir(tBolsa& bolsa, tSoportes& soportes);
+void ordenarPorColor(tSoporte& soporte);
+void ordenarPorNum(tSoporte& soporte);
+int buscar(const tJugada& jugada, const tFicha& ficha);
+void eliminaFichas(tSoporte& soporte, const tJugada& jugada);
+int nuevaJugada(tSoporte& soporte, tJugada& jugada);
 
-void recorrerBolsa(tFicha ficha, tBolsa& bolsa, int fila, int columna);
-
-void colorTexto(tColor color);
 
 
 int main()
@@ -63,7 +67,10 @@ int main()
 
 int menu()
 {
-
+	int opcion;
+	cout << "1: Ordenar por num., 2: Ordenar por color, 3: Sugerir, 4: Poner, 0: Fin >>> ";
+	cin >> opcion;
+	cout << opcion << endl;
 }
 
 void inicializarBolsa(tBolsa& bolsa)
@@ -87,26 +94,33 @@ tFicha robar(tBolsa& bolsa)
 {
 	bool encontrado = false;
 	tFicha ficha;
+	ficha.color = libre;
+	ficha.numero = -1;
 	int fila = rand() % 8, columna = rand() % NumFichas;
 	if (bolsa.bolsaFicha[fila][columna].color != libre)
 	{
 		ficha = bolsa.bolsaFicha[fila][columna];
+
+		bolsa.bolsaFicha[fila][columna].color = libre;
+		bolsa.bolsaFicha[fila][columna].numero = -1;
+
 		encontrado = true;
 	}
 	else
 	{
-		recorrerBolsa(ficha,bolsa, fila, columna);
+		encontrado = recorrerBolsa(bolsa, fila, columna, ficha);
+
 	}
 	if (!encontrado)
 	{
 		fila = 0;
 		columna = 0;
-		recorrerBolsa(ficha,bolsa, fila, columna);
+		encontrado = recorrerBolsa(bolsa, fila, columna, ficha);
 	}
 	return ficha;
 }
 
-void recorrerBolsa(tFicha ficha,tBolsa& bolsa, int fila, int columna)
+bool recorrerBolsa(tBolsa& bolsa, int& fila, int& columna, tFicha& ficha)
 {
 	bool encontrado = false;
 	while (!encontrado && fila < 8)
@@ -116,6 +130,8 @@ void recorrerBolsa(tFicha ficha,tBolsa& bolsa, int fila, int columna)
 			if (bolsa.bolsaFicha[fila][columna].color != libre)
 			{
 				ficha = bolsa.bolsaFicha[fila][columna];
+				bolsa.bolsaFicha[fila][columna].color = libre;
+				bolsa.bolsaFicha[fila][columna].numero = -1;
 				encontrado = true;
 			}
 			columna++;
@@ -123,19 +139,95 @@ void recorrerBolsa(tFicha ficha,tBolsa& bolsa, int fila, int columna)
 		columna = 0;
 		fila++;
 	}
+	return encontrado;
 }
 
-void repartir(tBolsa& bolsa, tSoportes& soportes)
-{
-	int numVueltas = 0;
-	for(int i = 0; i < NumJugadores; i++)
+void repartir(tBolsa& bolsa, tSoportes& soportes) {
+	int j = 0;
+	tFicha ficha;
+	for (int i = 0; i < NumJugadores; i++) 
 	{
-		for(int j = 0; j < IniFichas; j++)
+		while (soportes[i].contador < IniFichas) 
 		{
-			soportes[i].ficha = robar(bolsa);
+			ficha = robar(bolsa);
+			soportes[i].ficha[soportes[i].contador] = ficha;
 			soportes[i].contador++;
 		}
 	}
+}
+
+void ordenarPorNum(tSoporte& soporte)
+{
+	int  pos;
+	tFicha elemento;
+	for (int i = 0; i < MaxFichas; i++) 
+	{
+		pos = i;
+		elemento = soporte.ficha[pos];
+		while ((pos > 0) && (elemento.numero < soporte.ficha[pos - 1].numero)) 
+		{
+			soporte.ficha[pos] = soporte.ficha[pos - 1];
+			pos--;
+		}
+		soporte.ficha[pos] = elemento;
+	}
+
+}
+
+
+void ordenarPorColor(tSoporte& soporte)
+{
+	int  pos;
+	tFicha elemento;
+	for (int i = 0; i < MaxFichas; i++) 
+	{
+		pos = i;
+		elemento = soporte.ficha[pos];
+
+		while ((pos > 0) && (elemento.color < soporte.ficha[pos - 1].color)) 
+		{
+			soporte.ficha[pos] = soporte.ficha[pos - 1];
+			pos--;
+		}
+		soporte.ficha[pos] = elemento;
+	}
+}
+int buscar(const tJugada& jugada, const tFicha& ficha)
+{
+	bool encontrado = false;
+	int vueltas = 0, indice = -1;
+	while(!encontrado && vueltas < NumFichas + 1)
+	{
+		if(jugada[vueltas].color == ficha.color && jugada[vueltas].numero == ficha.numero)
+		{
+			indice = vueltas;
+			encontrado = true;
+		}
+		vueltas++;
+	}
+	return indice;
+}
+void eliminaFichas(tSoporte& soporte, const tJugada& jugada)
+{
+	for(int i = 0; i < soporte.contador; i++)
+	{
+		if(buscar(jugada, soporte.ficha[i]) != -1)
+		{
+			soporte.ficha[i].color = libre;
+			soporte.ficha[i].numero = -1;
+		}
+	}
+}
+
+int nuevaJugada(tSoporte& soporte, tJugada& jugada)
+{
+
+}
+
+void mostrarFicha(const tFicha& ficha)
+{
+	colorTexto(ficha.color);
+	cout << 
 }
 
 void colorTexto(tColor color)
@@ -158,4 +250,8 @@ void colorTexto(tColor color)
 		break;
 	}
 }
+
+
+
+
 
