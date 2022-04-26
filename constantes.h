eb6c1037e8901f50947d06fbced68a92fbb28127
JavierRDA01 <81706622+JavierRDA01@users.noreply.github.com>
2022-04-26 14:39:25 +0200

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+const int NumJugadores = 3;
+const int NumFichas = 9;
+const int IniFichas = 7;
+const int MaxFichas = 50;
+const int MaxJugadas = NumFichas * 2;
+#endif
