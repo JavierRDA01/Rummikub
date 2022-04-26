d3e1ce95d3b1cceab3d26c3f7e3718803fedd76b
JavierRDA01 <81706622+JavierRDA01@users.noreply.github.com>
2022-04-26 14:33:14 +0200

    Create colores.h

---
+++ colores.h
@@ -0,0 +1,8 @@
+#ifndef COLORES_H
+#define COLORES_H
+
+typedef enum { rojo, verde, azul, amarillo, blanco, libre }tColor;
+
+void colorTexto(tColor color);//Da color a la ficha según el tipo definido tColor
+
+#endif
