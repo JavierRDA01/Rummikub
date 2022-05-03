afb532e5f582a7adc4b3302089571c210cd2d8d5
hugag <102156712+hugag@users.noreply.github.com>
2022-05-03 11:11:15 +0200

    Update fichas.h

--- fichas.h
+++ fichas.h
@@ -1,12 +1,13 @@
-#include "colores.h"
+
 #ifndef FICHAS_H
 #define FICHAS_H
-
+#include "Colores.h"
+#include "Constantes.h"
 struct tFicha {
 	int numero = -1;
 	tColor color = libre;
 };
 
 typedef tFicha tArrayFichas[MaxFichas];
-
+void mostrarFicha(const tFicha& ficha);//Muestra el color y el número de una ficha
 #endif
